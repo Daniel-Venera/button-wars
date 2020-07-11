2eab41f5e138ee533b49a73753b0bc4c47564d89
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-11 10:32:44 +0200

    click 2 added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 let log = console.log;
 
 let burgerCross = document.querySelector(".menu-burger");
+let click2 = document.querySelector("#click2");
 
 burgerCross.addEventListener("click", function () {
   if (!burgerCross.classList.contains("open")) {
     burgerCross.classList.add("open");
   } else {
     burgerCross.classList.remove("open");
+  }
+});
+
+click2.addEventListener("click", function () {
+  if (click2.classList.contains("on")) {
+    click2.classList.remove("on");
+  } else {
+    click2.classList.add("on");
   }
 });
